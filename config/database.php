911ce58cb519714dc197e1ebb351025523fa7046
juanjo <juanjoulloa.mip@gmail.com>
2018-06-26 15:27:53 -0400

    fixed: env.

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'mysql',
-            'port' => '3306',
-            'database' => 'b-parking',
-            'username' => 'root',
-            'password' => '',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
